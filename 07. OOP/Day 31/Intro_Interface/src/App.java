11b1d568e805a4a0e386da562240719abe08b529
MD. SHAHRIAR AHAMMAD <callbackcrows7@gmail.com>
2024-10-05 18:34:54 +0600

    Learning Interface in java

---
+++ 07. OOP/Day 31/Intro_Interface/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Cat cat = new Cat();
+        cat.makeSound();
+
+        Dog dog = new Dog();
+        dog.makeSound();
+    }
+}
