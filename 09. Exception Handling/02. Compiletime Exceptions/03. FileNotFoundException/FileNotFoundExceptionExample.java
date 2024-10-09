2163f8589e4a502b5a90c24e214b4d40fa71bd42
MD. SHAHRIAR AHAMMAD <callbackcrows7@gmail.com>
2024-10-10 02:48:33 +0600

    Learning Compile Time Exception

---
+++ 09. Exception Handling/02. Compiletime Exceptions/03. FileNotFoundException/FileNotFoundExceptionExample.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileNotFoundExceptionExample {
+    public static void main(String[] args) {
+        try {
+            File file = new File("nonexistentfile.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: File not found! " + e.getMessage());
+        }
+    }
+}
